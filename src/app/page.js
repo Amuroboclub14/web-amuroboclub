070add61be4dde012a21a809dd40cbcba8f596c5
Sharjeel-Afridi <afridisharjeel8@gmail.com>
2024-10-15 23:02:48 +0530

    add: faq

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import cord from "../assets/coords.webp";
 import group from "../assets/IMG_2739.webp";
 import AnimatedSVG from "./components/AnimatedSvg";
 import * as motion from "framer-motion/client";
-
+import Faqs from "./components/Faqs";
 export default function Home() {
   return (
     <main>
@@ -240,6 +240,7 @@ export default function Home() {
           </motion.div>
         </div>
       </div>
+      <Faqs />
       <Footer />
     </main>
   );
