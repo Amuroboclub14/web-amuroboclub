1cd7f902bd96bfdceb2496df30f93f2aba514eee
Anushka <anushkavarshney2708@gmail.com>
2025-08-06 23:57:55 +0530

    fixed image name error

--- src/app/components/OpportunitiesOffered.tsx
+++ src/app/components/OpportunitiesOffered.tsx
@@ -1,7 +1,7 @@
 import OpportunityCard from "./ui/OpportunityCard";
 import StatBox from "./ui/StatBox";
 import Image from "next/image";
-import team from "../../assets/roboclub-coord.jpg";
+import team from "../../assets/roboclub-coordinators.jpg";
 export default function OpportunitiesOffered() {
   const opportunities = [
     {
