899453751b5383fc65ca122495a7dfd2171ed8a7
Anushka Varshney <anushkavarshney2708@gmail.com>
2025-09-27 14:11:49 +0530

    removed fallback use of env credentials

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -18,21 +18,5 @@ export async function POST(req) {
     return new Response(JSON.stringify({ success: true }), { status: 200 });
   }
 
-  // Fallback to environment variables
-  if (
-    username === process.env.ADMIN_USER &&
-    password === process.env.ADMIN_PASS
-  ) {
-    const cookieStore = cookies();
-    cookieStore.set({
-      name: "auth",
-      value: "true",
-      path: "/",
-      httpOnly: true,
-      maxAge: 60 * 60 * 24,
-    });
-    return new Response(JSON.stringify({ success: true }), { status: 200 });
-  }
-
   return new Response(JSON.stringify({ success: false }), { status: 401 });
 }
